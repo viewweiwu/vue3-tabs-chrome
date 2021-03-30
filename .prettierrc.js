19b8bfa9a7ed1914a965a02a3d6630aa8e083196
viewweiwu <648764190@qq.com>
2021-03-30 14:34:15 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  printWidth: 200, // 每行代码最大长度
+  tabWidth: 2, // 一个tab代表几个空格数，默认为80
+  useTabs: false, // 是否使用tab进行缩进，默认为false，表示用空格进行缩减
+  semi: false, // 声明后带分号
+  singleQuote: true, // 使用单引号
+  jsxSingleQuote: true, // 使用单引号
+  jsxBracketSameLine: true, // 启用jsx语法，> 放在末尾
+  trailingComma: 'none'
+}
