9db9834627b4587d3363c74daee60fc0b21b8198
flingyp <1213563369@qq.com>
2023-03-03 13:50:09 +0800

    fix: favicon type error

--- package/vue3-tabs-chrome.vue
+++ package/vue3-tabs-chrome.vue
@@ -59,15 +59,17 @@ import {
   nextTick,
   h,
   onUnmounted,
-  ComponentPublicInstance
+  ComponentPublicInstance,
+  VNode
 } from 'vue'
 
+export type FavicoType = ((...args: unknown[]) => VNode) | ((...args: unknown[]) => string) | NodeRequire | string
 export interface Tab {
   /** 显示名称 */
   label: string
   /** 唯一 key */
   key: string
-  favico?: string
+  favico?: FavicoType
   /**
    * 是否可关闭
    */
